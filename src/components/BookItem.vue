7c188df128ffbbceb48f31e0b6e4eb79437e2f8b
pardonmyfrench <34434265+pardonmyfrench@users.noreply.github.com>
2022-07-03 17:16:00 -0400

    Module 3

---
+++ src/components/BookItem.vue
@@ -0,0 +1,31 @@
+<template>
+<ul>
+<li v-for="book in books">{{book.title}}:{{book.author}}</li>
+</ul>
+</template>
+
+
+<script>
+
+export default{
+    name: "BookItem",
+    props: ['book']
+}
+
+
+</script>
+
+
+<style>
+h1, h2 {
+    font-weight: normal;
+}
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+li {
+    display: block;
+    margin: 0 10px;
+}
+</style>
