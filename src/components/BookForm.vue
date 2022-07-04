7f9996382bb480704975e59f2d700885042fa7ec
pardonmyfrench <34434265+pardonmyfrench@users.noreply.github.com>
2022-07-04 00:01:31 -0400

    Update BookForm.vue

--- src/components/BookForm.vue
+++ src/components/BookForm.vue
@@ -1,4 +1,10 @@
-<template></template>
+<template>
+    <form class="" action="#" method="post">
+        <input type="text" name="title" value="" placeholder="Book Title">
+        <input type="text" name="author" value="" placeholder="Book Author">
+        <button type="submit" name="button">Add Book</button>
+    </form>
+</template>
 
 <script>
 export default{
