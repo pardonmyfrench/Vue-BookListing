69a2de02b826c1bf6d3a883b99130ec9af49dca9
pardonmyfrench <34434265+pardonmyfrench@users.noreply.github.com>
2022-07-04 00:00:54 -0400

    Create BookForm.vue

---
+++ src/components/BookForm.vue
@@ -0,0 +1,17 @@
+<template></template>
+
+<script>
+export default{
+    name: "BookForm",
+    props: ['book'],
+    data () {
+        return{
+            bookTitle: '',
+            bookAuthor: ''
+        }
+    }
+}
+</script>
+
+
+<style></style>
